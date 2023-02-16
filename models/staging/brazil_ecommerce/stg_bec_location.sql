d18a5316bb62c1d1e66ab5433d524f8807a2c501
simon.jansen88@gmx.de <simon.jansen88@gmx.de>
2023-02-16 21:40:10 +0000

    added generic tests for BEC

--- models/staging/brazil_ecommerce/stg_bec_location.sql
+++ models/staging/brazil_ecommerce/stg_bec_location.sql
@@ -8,6 +8,8 @@ source as (
 bec_location as (
 
     select
+        -- concat latitude & longitude to unique surrogate key
+        concat(cast(geolocation_lat as varchar(50)), cast(geolocation_lng as varchar(50))) as location_id,
         geolocation_zip_code_prefix as zip_code,
         geolocation_lat as latitude,
         geolocation_lng as longitude,
